8ec7f2ef0d6d67277261c012045e1eb865f03230
Douglas Pew <douglas.pew@dichoda.com>
2017-12-10 21:29:48 -0500

    Completed day 2 correctly

--- day2/day2.py
+++ day2/day2.py
@@ -17,8 +17,29 @@ def chksum(*args):
         >>> chksum([5, 1, 9, 5], [7, 5, 3] , [2, 4, 6, 8])
         18
     '''
-    print args
     return sum(chkrow(*row) for row in args)
+
+def evendiv(*args):
+    '''
+         >>> evendiv(5, 9, 2, 8)
+         4
+         >>> evendiv(9, 4, 7, 3)
+         3
+         >>> evendiv(3, 8, 6, 5)
+         2
+    '''
+    for i in xrange(len(args)):
+        d = float(args[i])
+        remain=list(args)
+        del remain[i]
+        # print d, remain
+        for x in remain:
+           if x / d == int(x/d):
+               return int(x/d)
+    raise Exception("no divisor")
+
+def sumsum(*args):
+   return sum(evendiv(*row) for row in args)
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
@@ -27,5 +48,5 @@ if __name__ == '__main__':
     else:
         input=[]
         with open(sys.argv[1]) as f:
-            print chksum(*[[int(c) for c in r.split()] for r in f.readlines()])
+            print sumsum(*[[int(c) for c in r.split()] for r in f.readlines()])
     
