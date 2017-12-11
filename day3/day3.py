2e4c656007f883b6246701cbecccc5d3a810bba0
Douglas Pew <douglas.pew@dichoda.com>
2017-12-11 00:02:57 -0500

    Done with day3

--- day3/day3.py
+++ day3/day3.py
@@ -12,7 +12,7 @@ def spiral(maxval):
          [0, 7, 8, 9, 10],
          [0, 0, 0, 0, 0]]
     '''
-    m = Matrix(maxval)
+    m = Matrix(int(math.sqrt(maxval))+2)
     i = 0
     for p in spiralgen():
         i += 1
@@ -22,6 +22,23 @@ def spiral(maxval):
     
     return m
 
+def sumspiral(width, maxval):
+    '''
+        >>> sumspiral(5, 747)[0]
+        [[147, 142, 133, 122, 59],
+         [304, 5, 4, 2, 57],
+         [330, 10, 1, 1, 54],
+         [351, 11, 23, 25, 26],
+         [362, 747, 806, 0, 0]]
+    '''
+    m = Matrix(width)
+    m.set(0, 0, 1)
+    for p in spiralgen(): 
+        v = m.sumadj(p[0], p[1])
+        m.set(p[0], p[1], v)
+        if v > maxval:
+            return m, v
+        
 
 def spiralgen():
     x = 0
@@ -41,14 +58,32 @@ def spiralgen():
 
 class Matrix(object):
 
-    def __init__(self, size):
-        dim=int(math.sqrt(size))+2
+    def __init__(self, dim):
         line=[0 for x in xrange(dim)]
         self.matrix=[list(line) for x in xrange(dim)]
         self.center_x = self.center_y = dim/2
 
     def get(self, x, y):
-        return self.matrix[self.center_y - y][x + self.center_x[1]]
+        '''
+            >>> spiral(10).get(0, 0)
+            1
+            >>> spiral(10).get(1, 1)
+            3
+            >>> spiral(10).get(1, -1)
+            9
+            >>> spiral(10).get(10, -12)
+            0
+            >>> spiral(12).get(-3, 1)
+            0
+        '''
+        try:
+            x1 = self.center_x + x
+            y1 = self.center_y - y
+            if x1 < 0 or y1 < 0:
+                 return 0
+            return self.matrix[y1][x1]
+        except IndexError:
+            return 0
 
     def set(self, x, y, val):
         self.matrix[self.center_y - y][x + self.center_x] = val
@@ -73,6 +108,9 @@ class Matrix(object):
 
         raise KeyError(val) 
 
+    def sumadj(self, x, y):
+        return sum([self.get(x + x1, y + y1) for x1 in xrange(-1, 2) for y1 in xrange(-1, 2)])
+
     def __repr__(self):
         return pprint.pformat(self.matrix)
 
@@ -89,3 +127,7 @@ if __name__ == '__main__':
         p = s.find(val)
         print p
         print abs(p[0]) + abs(p[1])
+
+        s2, maxval = sumspiral(15, val) 
+        print s2
+        print maxval
