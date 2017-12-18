6ddd5108cb61dcfa47be4cdccb0e598a4f2ab16b
Douglas Pew <douglas.pew@dichoda.com>
2017-12-17 23:12:34 -0500

    Day 17 part 1 done

---
+++ day17/day17.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import sys
+
+class CircBuf(object):
+
+    def __init__(self, spin):
+        self.buffer = [ 0 ]
+        self.spin = spin
+        self.pos = 0
+
+    def add(self, num):
+        self.pos = (self.pos + self.spin) % len(self.buffer)
+        self.buffer.insert(self.pos+1, num)
+        self.pos += 1
+
+    def get(self, pos):
+        return self.buffer[pos % len(self.buffer)]
+
+    def __repr__(self):
+        return "{%s}" % (','.join(str(c) for c in self.buffer),)
+
+
+c = CircBuf(377)
+for x in xrange(1, 2018):
+    c.add(x)
+
+print c.get(c.pos + 1)
