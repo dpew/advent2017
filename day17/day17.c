cc8952cec517512914268aa2b75e38583a103ca4
Douglas Pew <douglas.pew@dichoda.com>
2017-12-18 08:58:15 -0500

    Day 17 C code part 1 working

---
+++ day17/day17.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef struct node * NodePtr;
+typedef struct node {
+    int val;
+    NodePtr next;
+} Node;
+
+
+void
+insert(NodePtr n, int val) {
+    NodePtr newnode = (NodePtr)malloc(sizeof(Node));
+    newnode->val = val;
+    newnode->next = n->next;
+    n->next = newnode;
+}
+
+NodePtr
+skip(NodePtr n, int skipval) {
+    for (int i=0;i <= skipval; i++)
+        n = n->next;
+    return n;
+}
+
+NodePtr
+find(NodePtr n, int val) {
+    while (n->val != val)
+        n = n->next;
+    return n;
+}
+
+int
+main(int argc, char *argv[]) {
+    int skipval = argc > 2 ? atoi(argv[1]) : 3;
+    int after = argc > 2 ? atoi(argv[2]) : 2017;
+    int count = argc > 3 ? atoi(argv[3]) : after;
+
+    // Initialize circular buffer
+    NodePtr circular = (NodePtr)malloc(sizeof(Node));
+    circular->next = circular;
+    circular->val = 0;
+    
+    // Insert
+    NodePtr current = circular;
+    for (int i = 1; i <= count; i++) {
+        current = skip(current, skipval);
+        insert(current, i);
+    }
+
+    // Find and print
+    NodePtr found = find(circular, after);
+    printf("%d %d\n", found->val, found->next->val);
+        
+    return 0;
+}
