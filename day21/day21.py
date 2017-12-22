65df7da7a2db294496b99b482cd059d1bd929f04
Douglas Pew <douglas.pew@dichoda.com>
2017-12-22 10:40:57 -0500

    Day 21 Attempt 2, fail so far

--- day21/day21.py
+++ day21/day21.py
@@ -43,21 +43,63 @@ def spinflip(gridstr):
        
     return list(set(d))
     
+def join(grids):
+    '''
+        >>> join(['12/56'])
+        '12/56'
+        >>> join(['12/56','34/78','ab/ef','cd/gh'])
+        '1234/5678/abcd/efgh'
+        >>> join(['12/56','34/78','ab/ef','cd/gh'])
+        '1234/5678/abcd/efgh'
+    '''
+    if len(grids) == 1:
+        return grids[0]
+
+    size=grids[0].count('/')+1
+    mlist = [np.matrix([list(elem) for elem in g.split('/')]) for g in grids]
+    size=len(mlist)/2
+    matrix = np.vstack([np.hstack(mlist[x*size:x*size+size]) for x in xrange(size)])
+    return '/'.join(''.join(m) for m in matrix.tolist())
+    
+
 def split(gridstr):
     '''
-        >>> split('1234/5678/abcd/efgh')
+        >>> [x for x in split('1234/5678/abcd/efgh')]
         ['12/56', '34/78', 'ab/ef', 'cd/gh']
-        >>> split('123/456/789')
+        >>> [x for x in split('123/456/789')]
         ['123/456/789']
+        >>> [x for x in split('123456/789ABC/DEFGHI/JKLMNO/PQRSTU/VWXYZa')]
+        ['12/78', '34/9A', '56/BC', 'DE/JK', 'FG/LM', 'HI/NO', 'PQ/VW', 'RS/XY', 'TU/Za']
     '''
     gridstr2 = gridstr.replace('/','')
-    if len(gridstr2) >= 16:
-        return [ "/".join((gridstr2[0:2], gridstr2[4:6])),
-                 "/".join((gridstr2[2:4], gridstr2[6:8])),
-                 "/".join((gridstr2[8:10], gridstr2[12:14])),
-                 "/".join((gridstr2[10:12], gridstr2[14:16]))]
+
+
+    matrix = s2m(gridstr)
+    if len(matrix) % 2 == 0:
+        divisor = 2
     else:
-        return [ gridstr ]
+        divisor = 3
+    size = int(len(matrix)/divisor)
+    # print "MATRIX",  matrix
+    # print size, divisor
+
+
+    for col in xrange(size):
+       for row in xrange(size):
+    #       print col*divisor,divisor*col+divisor, divisor*row,divisor*row+divisor
+           yield m2s(matrix[np.ix_(np.arange(col*divisor,col*divisor+divisor), np.arange(row*divisor,row*divisor+divisor))])
+ #          yield '/'.join(''.join(m) for m in matrix[np.ix_(np.arange(col*divisor,col*divisor+col), np.arange(row*divisor,row*divisor*row))].tolist())
+
+def m2s(m):
+    return '/'.join(''.join(m2) for m2 in m.tolist())
+
+def s2m(s):
+    '''
+        >>> m2s(s2m('12/34'))
+        '12/34'
+    '''
+    return np.matrix([list(x) for x in s.split('/')])
+
 
 mappings={}
 
@@ -65,6 +107,9 @@ def parse_line(line):
     x=re.split("[ =>]* ", line.strip())
     for perm in spinflip(x[0]):
         mappings[perm] = x[1]
+
+import doctest
+doctest.testmod()
 
 with open(sys.argv[1]) as f:
     for l in f.readlines():
@@ -75,8 +120,10 @@ grids = [ ".#./..#/###" ]
 for x in xrange(5):
     newgrids = []
     for g in grids:
-        newgrids.extend(split(mappings[g]))
-    grids=newgrids
+        newgrids.append(mappings[g])
+    print "newgrid", newgrids
+    print "join", s2m(join(newgrids))
+    grids=split(join(newgrids))
 
 count=0
 for g in grids:
