ec8cd1f73d14ecd34709693a15caf6ce4952243d
Douglas Pew <douglas.pew@dichoda.com>
2018-12-10 00:50:41 -0500

    Day9 part 2

--- 2018/day9/day9.py
+++ 2018/day9/day9.py
@@ -1,35 +1,45 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import sys
 import math
 import pprint
 import re
 import doctest
+from llist import dllist
 from collections import defaultdict
 
 
 class Marbles(object):
     def __init__(self):
-       self.marbles = [0]
+       self.marbles = dllist([0])
        self.cur = 0
+       self.node = self.marbles.nodeat(0) # None #self.marbles.nodeat(0)
 
     def add(self, marble):
-       a = 1 if len(self.marbles) < 1 else 2
-       self.cur = (self.cur + a)
-       if self.cur > len(self.marbles):
-          self.cur -= len(self.marbles)
-       self.marbles.insert(self.cur, marble)
+       a = 2 #1 if len(self.marbles) < 1 else 2
+       self.node = self.marbles.nodeat(0) if not self.node.next else self.node.next
+       #self.cur = (self.cur + a) % len(self.marbles)
+       #for x in range(1):
+#       if self.cur >= len(self.marbles):
+#          self.cur -= len(self.marbles)
+       self.node = self.marbles.insert(marble, self.node.next)
+       #self.node = self.marbles.nodeat(0) if not self.node.next else self.node.next
 
     def remove(self):
-       self.cur = self.cur - 7
-       if self.cur < 0:
-           self.cur += len(self.marbles)
-       return self.marbles.pop(self.cur)
+       for x in range(7):
+           self.node = self.marbles.last if not self.node.prev else self.node.prev
+       #self.cur = self.cur - 7
+       #if self.cur < 0:
+       #    self.cur += len(self.marbles)
+       #node = self.marbles.nodeat(self.cur)
+       node = self.node
+       self.node = self.marbles.nodeat(0) if not self.node.next else self.node.next
+       return self.marbles.remove(node)
 
     def __repr__(self):
        s = ""
        for e, m in enumerate(self.marbles):
-           if e == self.cur:
+           if m == self.node.value:
               s += " (%2d)" % (m, )
            else:
               s += "  %2d " % (m, )
@@ -54,6 +64,6 @@ if __name__ == '__main__':
           p[cp] += x + m.remove()
        else:
           m.add(x)
-       #print "[%2d]: %s" % (cp+1, m)
+       # print "[%2d]: %s" % (cp+1, m)
        cp = (cp + 1) % players
     print max(p)
