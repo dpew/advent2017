a39a316ddd22f520af753e06c716dad3ec6c8bb6
Douglas Pew <douglas.pew@dichoda.com>
2018-12-22 22:15:33 -0500

    2018 day 22 part 2 working

--- 2018/day22/day22p2.py
+++ 2018/day22/day22p2.py
@@ -56,18 +56,16 @@ def distance(p1, p2):
 
 
 class GeoIndex(object):
-    def __init__(self, width, height, depth):
+    def __init__(self, width, height, depth, target=None):
         self.width = width
         self.height = height
         self.depth = depth
+        self.target = target if target else (width, height)
 
-        self.geoindex = []
-        for y in range(height+1):
-           self.geoindex.append([0] * (width + 1))
-
+        self.geoindex = {}
         for y in range(height+1):
            for x in range(width + 1):
-              self.geoindex[y][x] = self.at(x, y)
+              self.geoindex[(x, y)] = self.at(x, y)
 
     def at(self, x, y):
         '''
@@ -83,13 +81,19 @@ class GeoIndex(object):
             >>> g.at(10, 10)
             510
         '''
-        if x == self.width and y == self.height:
+        if x == self.target[0] and y == self.target[1]:
             return self.erode(0)
         if x == 0:
             return self.erode(y * 48271)
         if y == 0:
             return self.erode(x * 16807)
-        return self.erode(self.geoindex[y-1][x] * self.geoindex[y][x-1])
+        try:
+            return self.geoindex[(x, y)]
+        except KeyError, e:
+        #    print "KEY ERROR", x, y
+        #    raise e
+            
+            return self.erode(self.at(x, y-1) * self.at(x-1, y))
 
     def erode(self, val):
         return (self.depth + val) % 20183
@@ -104,8 +108,8 @@ class GeoIndex(object):
             114
         '''
         risk=0
-        for y in range(self.height+1):
-            risk += sum(self.at(x, y) % 3 for x in range(self.width+1))
+        for y in range(self.target[1]+1):
+            risk += sum(self.at(x, y) % 3 for x in range(self.target[0]+1))
         return risk
 
     def choices(self, node, cost):
@@ -114,55 +118,61 @@ class GeoIndex(object):
              a node is a tuple of (pos, tool)
         '''
         pos, tool = node
+        mytype = self.type(*pos)
         
         for d in DIRS.values():
             p = addpos(pos, d)
-            if p[0] < 0 or p[0] > self.width or p[1] < 0 or p[1] > self.height:
+            if p[0] < 0 or p[1] < 0:
                 continue
-            newtype = self.type(*pos)
+            if p == self.target:
+                yield (cost + (1 if ('T' == tool) else 8), (p, 'T'))
+                return
+            newtype = self.type(*p)
             for newtool in TOOLS:
-                if newtool in ALLOWED[newtype]:
+                if newtool in ALLOWED[newtype] and newtool in ALLOWED[mytype]:
                     nc = (cost + (1 if (newtool == tool) else 8), (p, newtool))
                     # print nc
                     yield nc
 
     def mindist(self, source=(0, 0), target=None):
-        '''
-            >>> g = GeoIndex(10, 10, 510)
-            >>> cost, node = g.mindist()
-            >>> cost
-            45
-        '''
         if not target:
-            target = (self.width, self.height)
+            target = self.target
 
         totaldist = distance(source, target)
+        #print totaldist
 
         node=(source, 'T')
-        SEEN = defaultdict(lambda: 100000000000)
-        SEEN[node] = 0
+        SEEN = defaultdict(list)
+        #SEEN[node] = 0
         paths=[]
-        heappush(paths, (0, totaldist, node))
+        heappush(paths, (0, totaldist, node, None))
         # heappush(paths, (0, node))
+        maxdist = totaldist
 
         while True:
-            #p2 = list(paths)
-            #print [ heappop(p2) for i in range(len(p2)) ]
-            cost, dist, node = heappop(paths)
-            SEEN[node] = cost
-            #print cost, dist, node, len(paths)
+            cost, dist, node, parent = heappop(paths)
+            if node in SEEN:
+               continue
+            #print node
+            SEEN[node] = True # SEEN[parent] + [node]
+            #if dist < 2:
+                #print cost, dist, node, len(paths)
+                #p2 = list(paths)
+                #print [ heappop(p2) for i in range(min(len(p2), 10)) ]
+            #    maxdist = dist
             #print cost, node, len(paths)
 
             if node[0] == target:
-                return cost, node
+                return cost, node, SEEN[node]
 
            # returns list of (cost, node(pos, newtool))
             for newcost, newnode in self.choices(node, cost):
                 if newnode not in SEEN:
-                #if SEEN[newnode] > newcost:
-                     heappush(paths, (newcost, totaldist - distance(target, newnode[0]), newnode))
+#                     if newnode[0] != target or newnode[1] == 'T':
+                         heappush(paths, (newcost, distance(target, newnode[0]), newnode, node))
+#                     else:
+#                         print "SKIPPING", newnode
         #              heappush(paths, (newcost, newnode))
-                #     SEEN[newnode] = newcost
 
     def __repr__(self):
         out = ''
@@ -171,17 +181,47 @@ class GeoIndex(object):
             out = out + '\n'
         return out
 
+    def render(self, path):
+        el = dict(path)
+        out = ''
+        for y in range(self.height + 1):
+            out = out + ''.join(el[(x, y)] if (x, y) in el else TERRAIN[self.type(x, y)] for x in range(self.width+1))
+            out = out + '\n'
+        return out
+
+def pathcost(path):
+    p1 = path[0] 
+    changetool=0
+    move=0
+    for p2 in path[1:]:
+        move+=1
+        if p2[1] != p1[1]:
+            changetool +=1
+        p1 = p2
+        
+    return move, changetool, move + 7 * changetool
+
+
+if len(sys.argv) > 1:
+    import doctest
+
 
 if len(sys.argv) > 1:
     import doctest
     doctest.testmod()
     sys.exit(3)
 
-#depth=4845
-#target=(6,770)
-depth=510
-target=(10,10)
+depth=4845
+target=(6,770)
+#depth=510
+#target=(10,10)
+g = GeoIndex(2000, 2000, depth, target)
+#g = GeoIndex(20, 20, depth, target)
 print g.risk()
-g = GeoIndex(target[0], target[1], depth)
-print g.mindist()
+print g
+print "computing"
+cost, node, path = g.mindist()
+#print g.render(path)
+#print pathcost(path)
+print cost, node
 
